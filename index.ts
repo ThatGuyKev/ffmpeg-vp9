ea10e8cb9dd806634a6aca94360a19fc4bf5ab5c
Ahmed Abdalla <ahmed_aallah@hotmail.com>
2022-01-07 11:11:34 +0400

    export as lib

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
-import { scales } from './config'
-import Codec from './video2mpd'
-const init = async () => {
-    let video = new Codec("sample.mp4", scales)
+// import { scales } from './config'
+// import Codec from './video2mpd'
+// const init = async () => {
+//     let video = new Codec("sample.mp4", scales)
 
-    await video.encode().then(()=>{
-    video.createManifest()})
-}
-init()
+//     await video.encode().then(()=>{
+//     video.createManifest()})
+// }
+// init()
+
+export {default as Codec} from './video2mpd'
